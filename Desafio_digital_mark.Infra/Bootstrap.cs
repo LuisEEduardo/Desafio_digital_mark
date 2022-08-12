adecd233bb9ce5dba6c6ce9986418e2f6a7da730
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-08-12 10:27:18 -0300

    Adicionando camada de infra

---
+++ Desafio_digital_mark.Infra/Bootstrap.cs
@@ -0,0 +1,26 @@
+﻿using Desafio_digital_mark.Application.App;
+using Desafio_digital_mark.Application.Interface;
+using Desafio_digital_mark.Data;
+using Desafio_digital_mark.Data.Repositorio;
+using Desafio_digital_mark.Domain.Repositorio;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Desafio_digital_mark.Infra;
+
+public class Bootstrap
+{
+    public static void RegistroDeServicos(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped(typeof(IRepositorioBase<>), typeof(RepositorioBase<>));
+        services.AddScoped<IClienteRepositorio, ClienteRepositorio>();
+        services.AddScoped<IProjetoRepositorio, ProjetoRepositorio>();
+
+        services.AddScoped<IClienteAplicacao, ClienteAplicacao>();
+        services.AddScoped<IProjetoAplicacao, ProjetoAplicacao>();
+
+        services.AddDbContext<Contexto>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("Conexao")));
+    }
+}
