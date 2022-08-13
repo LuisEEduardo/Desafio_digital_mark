f1997763468f7ce7a2a0bb1db6cf3ad3c91f08e4
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-08-12 21:47:30 -0300

    Adicionando data annotations as view models

--- Desafio_digital_mark.Application/ViewModel/ClienteViewModel.cs
+++ Desafio_digital_mark.Application/ViewModel/ClienteViewModel.cs
@@ -1,7 +1,13 @@
-﻿namespace Desafio_digital_mark.Application.ViewModel;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Desafio_digital_mark.Application.ViewModel;
 
 public class ClienteViewModel
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [MinLength(1)]
+    [MaxLength(100)]
     public string Nome { get; set; }
 }
--- Desafio_digital_mark.Application/ViewModel/ProjetoViewModel.cs
+++ Desafio_digital_mark.Application/ViewModel/ProjetoViewModel.cs
@@ -1,11 +1,19 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Desafio_digital_mark.Application.ViewModel;
 
 public class ProjetoViewModel
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [MinLength(1)]
+    [MaxLength(100)]
     public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A tecnologia é obrigatória")]
+    [MinLength(1)]
+    [MaxLength(200)]
     public string Tecnologia { get; set; }
     public int ClienteId { get; set; }   
     public ClienteViewModel Cliente { get; set; }
